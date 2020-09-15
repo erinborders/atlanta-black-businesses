c5bf1a50b68bfd463c3ac0c7a1cd89d07e5a2fb4
Erin Borders <borders16@gmail.com>
2020-09-15 17:05:36 -0400

    added filters

--- Finder/Finder-Template/config.js
+++ Finder/Finder-Template/config.js
@@ -4,43 +4,47 @@ const config = {
     CSV: "https://docs.google.com/spreadsheets/d/1o1NuCmpCcJ2vLa4HwPGQoZSO82fEl_qweSJ5of_ve_0/gviz/tq?tqx=out:csv&sheet=atlantaBlackBusinesses",
     center: [-84.401, 33.760], //Lng, Lat
     zoom: 10, //Default zoom
-    title: "Replace with your title",
-    description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
+    title: "Buy Black",
+    description: "Want to support black companies? You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
     sideBarInfo: ["Name", "Business Type", "Offerings"],
-    popupInfo: ["Popup Information"],
+    popupInfo: ["Name"],
     filters: [
         {
             type: "dropdown",
             title: "Title of filter: ",
-            columnHeader: "Column Name",
+            columnHeader: "Business Type",
             listItems: [
-                'filter one',
-                'filter two',
-                'filter three',
-                'filter four',
-                'filter five',
-                'filter six',
-                'filter seven'
+                'Restaurant',
+                'Retail'
+                // 'filter three',
+                // 'filter four',
+                // 'filter five',
+                // 'filter six',
+                // 'filter seven'
             ]
         },
         {
             type: "checkbox",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
-            listItems: ["filter one", "filter two", "filter three"]
+            title: "Price: ",
+            columnHeader: "Price",
+            listItems: ["$", "$$", "$$$"]
         },
         {
             type: "dropdown",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
+            title: "Cuisine: ",
+            columnHeader: "Offerings",
             listItems: [
-                'filter one',
-                'filter two',
-                'filter three',
-                'filter four',
-                'filter five',
-                'filter six',
-                'filter seven'
+                'Breakfast',
+                'Pizza',
+                'Healthy',
+                'American',
+                'Southern',
+                'Caribbean',
+                'Ethiopian', 
+                'Bar',
+                'Seafood',
+                'Dessert',
+                'BBQ'
             ]
         }
     ]
